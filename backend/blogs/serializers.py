3efd4358752391940b57671d1a3500185ba8cda1
Sahil Kaushal <skaushal@breadware.com>
2020-01-24 15:19:32 +0530

    updated some backend

--- backend/blogs/serializers.py
+++ backend/blogs/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from . import models
 from core.apis.serializers import UserFieldModelSerializer
 
-class BlogSerializer(UserFieldModelSerializer):
+
+class GetBlogSerializer(UserFieldModelSerializer):
+
+    class Meta:
+        model = models.Blog
+        fields = ['user', 'title', 'description', 'id', 'image']
+
+
+class BlogSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Blog
--- backend/blogs/views.py
+++ backend/blogs/views.py
@@ -5,15 +5,22 @@ from . import serializers
 from . import models
 from . import permissions
 
-class BlogViewSet(viewsets.ModelViewSet):
-  authentication_classes = (TokenAuthentication,)
-  serializer_class = serializers.BlogSerializer
-  queryset = models.Blog.objects.all()
-  permission_classes = (
-    permissions.UpdateBlog,
-  )
-  filter_backends = (filters.SearchFilter,)
-  search_fields = ('user__id',)
 
-  def perform_create(self, serializer):
-    serializer.save(user=self.request.user)
+class BlogViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    queryset = models.Blog.objects.all()
+    permission_classes = (
+        permissions.UpdateBlog,
+    )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('user__id',)
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return serializers.GetBlogSerializer
+        if self.request.method == 'POST':
+            return serializers.BlogSerializer
+        return serializers.BlogSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
