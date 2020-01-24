bc9d15feba06846673cf7a393de8e8fb5cf383d6
Sahil Kaushal <skaushal@breadware.com>
2020-01-24 15:18:10 +0530

    update lifecycle methods

--- frontend/src/app/views/Blogs/components/Personal/index.tsx
+++ frontend/src/app/views/Blogs/components/Personal/index.tsx
@@ -4,6 +4,7 @@ import { getPersonalBlogs } from '../../requests';
 import { IOBlogCard } from '../../../../../lib/components/IOCards';
 import { IOLink } from '../../../../../lib/elements';
 import { blogsUrls } from '../../../../urls';
+import { IOHorizontalMenu } from '../../../../../lib/components/IOHorizontalMenu';
 
 interface PersonalBlogsProps {
   urlParams: {
@@ -15,7 +16,7 @@ export const PersonalBlogs: React.FC<PersonalBlogsProps> = ({ urlParams }) => {
   const [blogs, setBlogs] = React.useState<Blog[]>([] as Blog[]);
 
   React.useEffect(() => {
-    if(urlParams.id){
+    if (urlParams.id) {
       getPersonalBlogs(urlParams.id).then(
         res => setBlogs(res.data)
       )
@@ -25,7 +26,12 @@ export const PersonalBlogs: React.FC<PersonalBlogsProps> = ({ urlParams }) => {
   return (
     <div>
       <IOLink to={blogsUrls.createNewBlog()}>Create new blog</IOLink>
-      {blogs.map((blog, i) => <IOBlogCard key={i} blog={blog} />)}
+      <IOHorizontalMenu
+        menuItems={blogs}
+        renderMenuItem={(item: Blog) => <IOBlogCard blog={item} />}
+        numOfCards={5}
+        gutterWidth={'4px'}
+      />
     </div>
   )
 }
